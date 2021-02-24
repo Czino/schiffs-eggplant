afb03f6cc3e9e79045078ccbc362a79ef2e39f6f
Eric Lehmann <eric@machinas.com>
2021-02-24 18:15:53 +0100

    Delete config.mjs

--- config.mjs
+++
@@ -1,8 +0,0 @@
-export default {
-  twitterAPI: {
-    consumer_key: 'LCaRAM7YZX3U43uycsoNWu4bc',
-    consumer_secret: 'Vulbx59Usj6ALcakF2MmwQxRT8Icerq1nh2lzs1QoAHMhPSNL1',
-    access_token_key: '1364488716631371776-7Y4KBlCtF77eRthm0xXG1QuSy8psM9',
-    access_token_secret: 'yg91J8Xx1pATU0u9GA06ClCXYoaiYnBdrYnSYRDNETCsB'
-  }
-}
